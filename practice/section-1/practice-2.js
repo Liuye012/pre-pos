1d591750c7bf44cd2dfd4d356885f67d2cf0e76a
jian <Jiandandian012@outlook.com>
2015-07-23 13:03:46 +0800

    second

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,20 +1,18 @@
+var result = [];
 function collect_same_elements(collection_a, collection_b) {
-  var result = [];
   collection_a.forEach(function(val) {
-    var item = find_same_index(val, collection_b);
-
- result.push(item);
-
+     find_same_index(val, collection_b);
   })
   return result;
 }
 
 function find_same_index(m, collection_b) {
-  collection_b.forEach(function(val) {
-    var k = val.indexOf(m);
-    if (k ) {
-      return k;
-
+    for (var i = 0; i < collection_b.length; i++) {
+      for (var j = 0; j < collection_b[i].length; j++) {
+        if(m===collection_b[i][j]){
+          result.push(m);
+        }
+      }
     }
-  })
+    return result;
 }
