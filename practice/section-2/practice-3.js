47d5766d223e70ef203844fe7e7c132f1853f04b
jian <Jiandandian012@outlook.com>
2015-07-23 18:06:48 +0800

    no change

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,3 +1,27 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var result = [];
+  collection.forEach(function(val) {
+       check_elements(val,result);
+  });
+  return result;
+}
+
+function check_elements(val, result) {
+  var cnt;
+  for (var i = 0, cnt = 0; i < result.length; i++) {
+    if (val.charAt(0) === result[i].key) {
+      result[i].count += val.indexOf("[") || val.indexOf(":") || val.indexOf("-") > -1 ? parseInt(val.charAt(2)) : 1;
+      cnt++;
+    }
+  }
+  if (cnt === 0) {
+    new_Object(val, result);
+  }
+}
+
+function new_Object(v, result) {
+  var m = {};
+  m.key = v.charAt(0);
+  m.count = v.indexOf("[") || v.indexOf(":") || v.indexOf("-") > -1? parseInt(v.charAt(2) ): 1;
+  result.push(m);
 }
